b650000634162d9d4d8ca9bfdf9ab05f8fb9786a
Gabriel Carlos <gabrielcarlos0705@gmail.com>
2025-07-04 17:50:39 -0300

    feat: ask AI with rag context

--- main.go
+++ main.go
@@ -11,18 +11,6 @@ import (
 )
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-	// query?prompt=something
-	knowledge, err := knowledge.LoadFromDirectory("./guides")
-	if err !=nil {
-		log.Printf("Error loading knowlodge base: ", err)
-		http.Error(w,"Could not load knowledge base.", http.StatusInternalServerError)
-		return
-	} 
-	
-	log.Println("Knowledge base loaded succesfully...")
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprint(w, knowledge)
-
 	prompt := r.URL.Query().Get("prompt")
 	if prompt == "" {
 		http.Error(w, "Error: 'prompt' parameter is required.", http.StatusBadRequest)
@@ -30,14 +18,37 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	log.Printf("Prompt received: \"%s\"", prompt)
 
-	response, err := gemini.Ask(prompt)
+	knowledgeBase, err := knowledge.LoadFromDirectory("./guides")
+	if err != nil {
+		log.Printf("Error loading knowledge base: %v", err)
+		http.Error(w, "Could not load knowledge base.", http.StatusInternalServerError)
+		return
+	}
+
+	context := knowledge.RetrieveContext(prompt, knowledgeBase)
+	if context == "" {
+		context = "No relevant context found."
+	}
+
+	finalPrompt := fmt.Sprintf(`
+		You will be the AI ​​agent for XXX . You will be the first contact to try to solve a problem sent by the customer. This message has a context in which we searched our internal documentation to help you answer the question. Based on this document, reply the costumer question, if you don't know the answer, say that isn't in your documentation. Answer in PT-BR
+
+		Context:
+		%s
+
+		Question:
+		%s
+	`, context, prompt)
+
+
+	response, err := gemini.Ask(finalPrompt)
 	if err != nil {
 		log.Printf("Error calling gemini package: %v", err)
 		http.Error(w, "Error processing request with Gemini.", http.StatusInternalServerError)
 		return
 	}
 
-	log.Println("Response sent successfully!")
+	log.Println("Context-aware response sent successfully!")
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprint(w, response)
 }
